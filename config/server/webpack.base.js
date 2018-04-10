d9940fb6a41ee08f552dabcc55224951a211d44a
Onur Elibol <korax11@hotmail.com>
2018-04-10 14:33:21 +0200

    Move utils file

--- config/server/webpack.base.js
+++ config/server/webpack.base.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const utils = require("../utils");
+const utils = require("./utils");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const nodeModules = utils.getExternalNodeModules();

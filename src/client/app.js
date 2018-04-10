0f567e782df3cf334af47bb27796eb1916e72e2b
Onur Elibol <korax11@hotmail.com>
2018-04-10 14:02:44 +0200

    Load scss globally

--- src/client/app.js
+++ src/client/app.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { sync } from "vuex-router-sync";
+import "./style/global.scss";
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);

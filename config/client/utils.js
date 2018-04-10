4d377034129372e44a32c86d62845a6485555176
Onur Elibol <korax11@hotmail.com>
2018-04-10 14:02:15 +0200

    Change globally loaded scss file / refactor

--- config/client/utils.js
+++ config/client/utils.js
@@ -21,37 +21,14 @@ const sassLoader = {
 const sassResLoader = {
   loader: "sass-resources-loader",
   options: {
-    resources: path.resolve(__dirname, "../../src/client/style/global.scss")
+    resources: path.resolve(__dirname, "../../src/client/style/vars.scss")
   }
 };
 
-const vueScssPROD =  ExtractTextPlugin.extract({
+const vueScssPROD = ExtractTextPlugin.extract({
   fallback: "vue-style-loader",
-  use: [
-    {
-      loader: "css-loader",
-      options: {
-        sourceMap: "production" === process.env.NODE_ENV ? false : true,
-        options: { minimize: true },
-        importLoaders: 1
-      }
-    },
-    {
-      loader: "sass-loader",
-      options: {
-        sourceMap: "production" === process.env.NODE_ENV ? false : true,
-        outputStyle: "production" === process.env.NODE_ENV ? "compressed" : "nested"
-      }
-    },
-    {
-      loader: "sass-resources-loader",
-      options: {
-        resources: path.resolve(__dirname, "../../src/client/style/global.scss")
-      }
-    }
-  ]
+  use: [cssLoader, sassLoader, sassResLoader]
 });
-
 const vueScssDEV = ["vue-style-loader", cssLoader, sassLoader, sassResLoader];
 
 // Vue loaders

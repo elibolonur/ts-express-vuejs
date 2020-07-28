15fa5d451d9517744b596d34fcec564dd638ef3f
Onur Elibol <korax11@hotmail.com>
2020-07-28 18:38:07 +0200

    lint

--- server/app.ts
+++ server/app.ts
@@ -14,7 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", home.index);
 
 app.listen(app.get("port"), () => {
-  console.log("App is running at http://localhost:%d in %s mode", app.get("port"), app.get("env"));
+  console.log(
+    "App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
   console.log("Press CTRL-C to stop\n");
 });
 
